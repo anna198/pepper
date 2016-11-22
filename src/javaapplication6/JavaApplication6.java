7ac2a6534cdd30786999ab451cadb32d481bd003
1 <1@172.20.10.3>
2016-11-22 09:19:45 +0200

    4456

---
+++ src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author 1
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            /*int p1 = 6;
+        int kvadrat = 2;
+        System.out.println("Результат возведения в квадрат: " + Math.pow(p1, kvadrat));
+        System.out.println("*****result: " + Math.sqrt(Math.pow(p1, kvadrat)));*/
+        // TODO code application logic here
+       /* int s;
+        int s1 = 72;
+        int s2 = 8;
+        s = s1-s2;
+        System.out.println("result: " + s);
+        System.out.println("*****result: " + Math.sqrt(s));*/
+       double[] mas1 = {5.6, 2, 7.4};
+       double[] mas2 = {11, 7.8, 4.1};
+       double[][] mas3 = new double[mas1.length][mas2.length];
+        System.out.println(mas3[0][0]);
+        System.out.println(mas3[1][1]);
+        System.out.println(mas3[2][2]);
+    }
+
+}
